054b8d63f8ab192b9e12f3fbfe6789f15534d3cf
krasikov.ev <evkrasikov87@gmail.com>
2025-11-26 15:08:04 +0300

    add PartnerUpdate

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,110 @@
 from django.shortcuts import render
+from distutils.util import strtobool
+from rest_framework.request import Request
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+from django.db import IntegrityError
+from django.db.models import Q, Sum, F
+from django.http import JsonResponse
+from requests import get
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from ujson import loads as load_json
+from yaml import load as load_yaml, Loader
+
+from backend.models import Shop, Category, Product, ProductInfo, Parameter, ProductParameter, Order, OrderItem, \
+    Contact, ConfirmEmailToken
+from backend.serializers import UserSerializer, CategorySerializer, ShopSerializer, ProductInfoSerializer, \
+    OrderItemSerializer, OrderSerializer, ContactSerializer
+from backend.signals import new_user_registered, new_order
 
 # Create your views here.
+class PartnerUpdate(APIView):
+    """
+    A class for updating partner information.
+
+    Methods:
+    - post: Update the partner information.
+
+    Attributes:
+    - None
+    """
+
+    def post(self, request, *args, **kwargs):
+        """
+                Update the partner price list information.
+
+                Args:
+                - request (Request): The Django request object.
+
+                Returns:
+                - JsonResponse: The response indicating the status of the operation and any errors.
+                """
+        # Аутентинтификация
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
+
+        if request.user.type != 'shop':
+            return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
+
+        url = request.data.get('url')
+        # Валидация URL
+        if url:
+            validate_url = URLValidator()
+            try:
+                validate_url(url)
+            except ValidationError as e:
+                return JsonResponse({'Status': False, 'Error': str(e)})
+            else:
+                # Загрузка YAML
+
+                try:
+                    stream = get(url, timeout=20).content
+                    data = load_yaml(stream, Loader=Loader)
+                except Exception as e:
+                    return JsonResponse({'Status': False, 'Error': f'Ошибка загрузки файла: {str(e)}'})
+                
+                required_fields = ['shop', 'categories', 'goods']
+                for field in required_fields:
+                    if field not in data:
+                        return JsonResponse({
+                            'Status': False, 
+                            'Error': f'Отсутствует обязательное поле: {field}'
+                        }, status=400)
+                # stream = get(url).content
+                # data = load_yaml(stream, Loader=Loader)
+
+                # Импорт магазина
+                shop, _ = Shop.objects.get_or_create(name=data['shop'], user_id=request.user.id)
+                # Импорт категорий магазина
+                for category in data['categories']:
+                    category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+                    category_object.shops.add(shop.id)
+                    category_object.save()
+                #Удаляем все предыдущие предложения товаров этого магазина перед новым импортом
+                ProductInfo.objects.filter(shop_id=shop.id).delete()
+                # Импорт товаров
+                for item in data['goods']:
+                    product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+
+                    product_info = ProductInfo.objects.create(product_id=product.id,
+                                                              external_id=item['id'],
+                                                              model=item['model'],
+                                                              price=item['price'],
+                                                              price_rrc=item['price_rrc'],
+                                                              quantity=item['quantity'],
+                                                              shop_id=shop.id)
+                    # Обработка параметров товаров
+                    for name, value in item['parameters'].items():
+                        parameter_object, _ = Parameter.objects.get_or_create(name=name)
+                        ProductParameter.objects.create(product_info_id=product_info.id,
+                                                        parameter_id=parameter_object.id,
+                                                        value=value)
+
+                return JsonResponse({'Status': True})
+
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
